962d7c2031da8e04729216136783caf7282b390a
derkant <grip4.pasto@gmail.com>
2021-02-17 00:08:36 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-ldxuxv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-ldxuxv)
---
+++ src/app/bo/ui.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "bo",
+  templateUrl: "./ui.component.html"
+})
+export class BackOffice {}
